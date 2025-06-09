9a6e768152ed93926a1e73300670665b8ef26dfc
Amar2K111 <amarcontactpro40@gmail.com>
2025-06-09 10:27:28 +0200

    Make logo smaller and white outline thicker

--- OneDrive/Bureau/tambo/tambo/src/components/Logo.tsx
+++ OneDrive/Bureau/tambo/tambo/src/components/Logo.tsx
@@ -6,13 +6,17 @@ interface LogoProps {
 
 export const Logo: React.FC<LogoProps> = ({ className = '' }) => {
   return (
-    <div className={`text-3xl font-extrabold text-[#2686F8] tracking-tight select-none ${className}`}
+    <div className={`text-2xl font-extrabold text-[#2686F8] tracking-tight select-none ${className}`}
       style={{
         textShadow: `
-          -1px -1px 0 #fff,
-          1px -1px 0 #fff,
-          -1px 1px 0 #fff,
-          1px 1px 0 #fff
+          -2px -2px 0 #fff,
+          2px -2px 0 #fff,
+          -2px 2px 0 #fff,
+          2px 2px 0 #fff,
+          0px 2px 0 #fff,
+          2px 0px 0 #fff,
+          0px -2px 0 #fff,
+          -2px 0px 0 #fff
         `
       }}
     >
